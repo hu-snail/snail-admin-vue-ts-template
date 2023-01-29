0dede16a31d5be2c26492c0d5f68413444201f2b
hujiangjun <1217437592@qq.com>
2023-01-29 11:27:08 +0800

    ✨feat(custom): new project settings

---
+++ stylelint.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    customSyntax: "postcss-html",
+    extends: ["stylelint-config-standard", "stylelint-config-recess-order", "stylelint-config-prettier"],
+    ignoreFiles: ["**/*.js", "**/*.jsx", "**/*.tsx", "**/*.ts", "**/*.json"],
+};
